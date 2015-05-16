3d8307c55f8b39e2e10c10e5c7af3f704109dec2
Matthew Fournier <fournier.matthew@gmail.com>
2015-05-16 08:55:19 -0400

    write the predict function
    Issue: master

--- src/logistic.py
+++ src/logistic.py
@@ -1,11 +1,25 @@
 from math import exp
+from numpy import vectorize
 
-def cost():
+
+def cost(X, y, theta):
     pass
 
 
+def predict(X, theta):
+    """Create a vector of predictions.
+
+    :param X: a matrix containing the input data
+    :type X: numpy.matrix
+    :param theta: a column vector containing the model parameters
+    :type theta: numpy.matrix
+    :rtype: numpy.matrix
+    """
+    return vectorize(sigmoid)(X*theta)
+
+
 def sigmoid(x):
-    """Calculate the sigmoid function for x. 
+    """Calculate the sigmoid function for x.
 
     :param x: a number
     :type x: float
