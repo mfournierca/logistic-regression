141e44d4352d2bca2274ac0e9d2c9b77dd379943
Matthew Fournier <fournier.matthew@gmail.com>
2015-05-16 09:01:51 -0400

    improve docstrings
    Issue: master

--- src/logistic.py
+++ src/logistic.py
@@ -3,15 +3,29 @@ from numpy import vectorize
 
 
 def cost(X, y, theta):
+    """Compute the cost for a given value of the training data and parameters.    
+    
+    :param X: An n x m  matrix containing the input data
+    :type X: numpy.matrix
+    :param y: A row vector of length m containing the predictions of the 
+        training set
+    :type y: numpy.matrix
+    :param theta: A column vector of length n containing the model parameters
+    :type theta: numpy.matrix
+    """
+    pass
+
+
+def regularized_cost():
     pass
 
 
 def predict(X, theta):
     """Create a vector of predictions.
 
-    :param X: a matrix containing the input data
+    :param X: An n x m matrix containing the input data
     :type X: numpy.matrix
-    :param theta: a column vector containing the model parameters
+    :param theta: A column vector of length m containing the model parameters
     :type theta: numpy.matrix
     :rtype: numpy.matrix
     """
@@ -27,6 +41,3 @@ def sigmoid(x):
     """
     return 1.0 / (1.0 + exp(-1.0 * x))
 
-
-def regularized_cost():
-    pass
